5a40f9571c1cda04092c378589d2356340c35938
Mateo Medranda <matemedranda15@gmail.com>
2024-06-29 19:26:20 -0500

    1.1.0

---
+++ code/chatbot_Main.py
@@ -0,0 +1,13 @@
+import nltk
+import numpy
+import tflearn
+import tensorflow
+import json
+import random
+import pickle
+from nltk.stem.lancaster import LancasterStemmer
+stemmer = LancasterStemmer
+
+nltk.download('punkt')
+
+
