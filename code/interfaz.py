b518225c1b82b1146ef9fa41155f08bd4b5025c8
AlissonAyo <alisson.ayo15@gmail.com>
2024-07-10 12:02:00 -0500

    2.4.0

---
+++ code/interfaz.py
@@ -0,0 +1,33 @@
+from tkinter import *
+
+def Abrir_ventana():
+    ventana= Toplevel(raiz)
+    ventana.title("Amaya")
+    ventana.config(bg="sky blue")
+    
+    area_Mensaje = Text(ventana, wrap=WORD, width=50, height=20, bg="light yellow")
+    area_Mensaje.pack(padx=10, pady=10)
+    
+    mensaje_entrada = Entry(ventana, width=40)
+    mensaje_entrada.pack(side=LEFT, padx=10, pady=10)
+    
+    enviar_Button = Button(ventana, text="Enviar", command=lambda: enviar_Mensaje(area_Mensaje, mensaje_entrada))
+    enviar_Button.pack(side=LEFT, padx=10, pady=10)
+
+def enviar_Mensaje(chat_area, mensaje_entrada):
+    mensaje_usuario = mensaje_entrada.get()
+    if mensaje_usuario.strip():
+        chat_area.insert(END, "Tú: " + mensaje_usuario + "\n")
+        mensaje_entrada.delete(0, END)
+        chat_area.insert(END, "Amaya: Este es un mensaje de respuesta.\n")
+        chat_area.see(END)
+
+raiz = Tk()
+raiz.title("Chat Bot")
+raiz.config(bg="sky blue")
+
+Label(raiz, text="Habla con Amaya", bg="sky blue", font=("courier new", 14)).pack(pady=50,padx=100)
+bonotn=Button(raiz, text="OK", command= Abrir_ventana).pack(pady=50,padx=100)
+
+
+raiz.mainloop()
