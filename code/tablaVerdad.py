76925fb27349f1cddf5f48c5e5a26b35ab4d1204
Mateo Medranda <matemedranda15@gmail.com>
2024-07-10 21:38:52 -0500

    3.0.0

--- code/tablaVerdad.py
+++ code/tablaVerdad.py
@@ -1,4 +1,5 @@
 from itertools import product
+from collections import deque
 
 def generar_valores_verdad(n):
     combinaciones = list(product([True, False], repeat=n))
@@ -13,6 +14,48 @@ def generar_valores_verdad(n):
 def implica(A,B):
      pass
 
+def obtenerPrecedencia(caracter):
+     if(caracter == '~'):
+          return 6
+     elif(caracter == '∧'):
+          return 5
+     elif(caracter == '∨'):
+          return 4
+     elif(caracter == '⊕'):
+          return 3
+     elif(caracter == '→'):
+          return 2
+     elif(caracter == '↔'):
+          return 1
+
+def esOperador(caracter):
+     caracteres ={'~', '∧','∨', '⊕', '→','↔'}
+     if(caracter in caracteres):
+          return True
+
+def infijaPostfija(cadena):
+    pila = deque()
+    postfija = []
+
+    for caracter in cadena:
+        if caracter.isalnum():
+            postfija.append(caracter)
+        elif(caracter == '('):
+            pila.append(caracter)
+        elif(caracter == ')'):
+            while(pila and pila[-1] != '('):
+                 postfija.append(pila.pop())
+            pila.pop()
+        elif(esOperador(caracter)):
+            while(pila and obtenerPrecedencia(pila.top()) >= obtenerPrecedencia(caracter)):
+                  postfija.append(pila.pop())
+            pila.append(caracter)
+
+    while(pila):
+        postfija.append(pila.pop())
+
+    return postfija
+
 def tablaVerdad():
     print("Ingrese la sintaxis haciendo uso de paréntesis para la precedencia Ej. (A^B)→C:")
     variables = []
@@ -23,8 +66,7 @@ def tablaVerdad():
 	        variables.append(i.upper())
     
     valores_logicos = generar_valores_verdad(len(variables))
-    
-
+    print(infijaPostfija(funcion))
 
 tablaVerdad()
 
